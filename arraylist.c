24f0aebf9d10256118df7a0d4972435c17f52c1f
Replit user <>
2023-04-01 19:40:15 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -54,9 +54,6 @@ void* get(ArrayList * l, int i) {
   if (i < 0) {
     i += l->size;  // convertir el índice negativo en uno positivo    
   }
-  if (i < 0 || i >= l->size) {
-       return NULL;  // índice fuera de rango
-  }
   return l->data[i];
 }
 
